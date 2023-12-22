fc4414a5c01c423576fa77a461e72822bee6b2ca
natalia lucero <nlucero@13.cl>
2023-12-22 16:57:41 -0300

    ultimo commit del día

--- js/app3.js
+++ js/app3.js
@@ -1,5 +1,5 @@
 // pedido de producto
-let producto = parseInt(prompt('Elige tus ovillos: \n 1-Acrílico \n 2-Lana \n 3-Bambú \n 4-Algodón \n 5-Ver pedido \n 6-Salir'));
+
 let pedidos = []
 
 //Class
@@ -54,31 +54,33 @@ function algodon() {
 }
 
 function verPedidos () {
-    let ovillo = prompt('Ingresa la composición que deseas filtrar: \n Acrílico \n Lana \n Bambú \n Algodón');
+    const ovillo = prompt('Ingresa la composición que deseas filtrar: \n acrílico \n lana \n bambú \n algodón');
     const filtroDePedidos = pedidos.filter((elemento)=> {
-        console.log('hola')
        return elemento.ovillo === ovillo
     });
 
-    filtroDePedidos.forEach(element => {
-        console.log(element.color + element.grosor)
-        return element.color + element.grosor 
+    filtroDePedidos.forEach(pedidoOvillos => {
+        console.log('hola')
+        //return element.color + element.grosor 
     });
 }
-
+let producto = parseInt(prompt('Elige tus ovillos: \n 1-Acrílico \n 2-Lana \n 3-Bambú \n 4-Algodón \n 5-Ver pedido \n 6-Salir'));
 while (producto !== 7  ) {
     if(producto === 1 ) {
         acrilico();
+
     }else if(producto === 2 ) {
         lana();
 
     }else if (producto === 3){
         bambu()
-        break
+
     }else if(producto === 4) {
         algodon()
+
     }else if(producto === 5) {
         verPedidos()
+
     }else if(producto=== 6) {
         alert('Salir')
         break;
